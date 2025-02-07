7d30bd75e953af14890e9f42231a1d9873f61b89
Lauren Kasper <lauren.n.kasper@vanderbilt.edu>
2025-02-07 11:34:02 -0500

    merged pions flag added

--- trd_mlp_fermi.C
+++ trd_mlp_fermi.C
@@ -31,6 +31,7 @@
 //#define VERBOSE
 #define ANALYZE_MERGED 1
 #define NO_RAD_COMPARE 1
+//#define ALL_PION_COMPARE 1
 
 void Count(const char *tit);
 void Count(const char *tit, double cut1);
@@ -335,13 +336,13 @@ int fill_trees(TTree *ttree_hits, TTree *signal, TTree *background, TTree *sig_t
       case 3197:   tw1=107; tw2=148; tw3=186; e_chan1=93;   e_chan2=147;  pi_chan1=e_chan1;   pi_chan2=e_chan2;   break; //-- Double Foil
       case 3198:   tw1=107; tw2=148; tw3=186; e_chan1=93;   e_chan2=147;  pi_chan1=e_chan1;   pi_chan2=e_chan2;   break; //-- Double Foil
       case 3199:   tw1=107; tw2=148; tw3=186; e_chan1=93;   e_chan2=147;  pi_chan1=e_chan1;   pi_chan2=e_chan2;   break; //-- Double Foil
-      case 3200:   tw1=109; tw2=146; tw3=182; e_chan1=94;   e_chan2=150;  pi_chan1=e_chan1;   pi_chan2=e_chan2;   break; //-- Double Foil
+      case 3200:   tw1=109; tw2=143; tw3=182; e_chan1=94;   e_chan2=150;  pi_chan1=e_chan1;   pi_chan2=e_chan2;   break; //-- Double Foil
       
       case 3201:   tw1=107; tw2=148; tw3=186; e_chan1=89;   e_chan2=148;  pi_chan1=e_chan1;   pi_chan2=e_chan2;   break; //-- Double Fleece
-      case 3202:   tw1=109; tw2=146; tw3=182; e_chan1=94;   e_chan2=150;  pi_chan1=e_chan1;   pi_chan2=e_chan2;   break; //-- Double Fleece
+      case 3202:   tw1=109; tw2=143; tw3=182; e_chan1=94;   e_chan2=150;  pi_chan1=e_chan1;   pi_chan2=e_chan2;   break; //-- Double Fleece
       
       case 3203:   tw1=107; tw2=148; tw3=186; e_chan1=89;   e_chan2=148;  pi_chan1=e_chan1;   pi_chan2=e_chan2;   break; //-- No Rad
-      case 3204:   tw1=109; tw2=146; tw3=182; e_chan1=94;   e_chan2=150;  pi_chan1=e_chan1;   pi_chan2=e_chan2;   break; //-- No Rad
+      case 3204:   tw1=109; tw2=143; tw3=182; e_chan1=94;   e_chan2=150;  pi_chan1=e_chan1;   pi_chan2=e_chan2;   break; //-- No Rad
       
       case 3287:   tw1=108; tw2=153; tw3=184; e_chan1=90;   e_chan2=145;  pi_chan1=e_chan1;   pi_chan2=e_chan2;   break; //-- Single Foil (TU)
       case 3288:   tw1=108; tw2=153; tw3=184; e_chan1=90;   e_chan2=145;  pi_chan1=e_chan1;   pi_chan2=e_chan2;   break; //-- Single Foil (TU)
@@ -642,6 +643,8 @@ void trd_mlp_fermi(int RunNum) {
   #if ANALYZE_MERGED
     #if NO_RAD_COMPARE
       sprintf(rootfile,"RootOutput/fermiMerged/nrc_trd_singleTrackHits_Run_%06d_%06dEntries.root",RunNum,nEntries);
+    #elif ALL_PION_COMPARE
+      sprintf(rootfile,"RootOutput/fermiMerged/allPi_trd_singleTrackHits_Run_%06d_%06dEntries.root",RunNum,nEntries);
     #else
       sprintf(rootfile,"RootOutput/fermiMerged/trd_singleTrackHits_Run_%06d_%06dEntries.root",RunNum,nEntries);
     #endif
